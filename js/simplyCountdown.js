a1e19bcb022f8c63e0fb453d0f09b6e7cf1e5510
Mahmud Yunus <mahmuyun@gmail.com>
2018-05-14 07:40:08 +0700

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2018,
+                month: 7,
+                day: 7,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
